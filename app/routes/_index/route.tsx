a36f2d342a342cbae1192415b7e289b1eeddd5b8
SunnyBiolie <nguyenthanhdanh1789@gmail.com>
2024-04-04 18:32:12 +0700

    Add Modal to announce about Thin Backend error(s)

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -2,7 +2,7 @@ import { Link } from "@remix-run/react";
 import Header from "../../components/for-cover/header";
 import Footer from "../../components/for-cover/footer";
 import { Button } from "~/components/ui/button";
-import { NotebookText, PencilLine } from "lucide-react";
+import { NotebookText, PencilLine, XIcon } from "lucide-react";
 
 const IndexPage = () => {
   return (
@@ -20,7 +20,7 @@ const IndexPage = () => {
             asChild
             className="flex items-center justify-center gap-2 py-3 "
           >
-            <Link to={"/write"} prefetch="render">
+            <Link to={"/write"} /* prefetch="render" */>
               <PencilLine className="w-4 h-4" /> Start write
             </Link>
           </Button>
@@ -42,6 +42,42 @@ const IndexPage = () => {
         </div>
       </div>
       <Footer />
+      <div
+        id="temp-notice"
+        className="fixed top-0 bottom-0 left-0 right-0 bg-neutral-950/50 flex items-center justify-center shadow-md"
+      >
+        <div className="relative bg-neutral-900 p-6 rounded-md w-5/6 md:w-4/5 lg:w-3/4 xl:w-1/2">
+          <button
+            className="absolute top-4 right-4 rounded-full hover:bg-neutral-500/50 p-1.5 transition cursor-pointer flex items-center justify-center"
+            onClick={() => {
+              document.getElementById("temp-notice")?.classList.add("hidden");
+            }}
+          >
+            <XIcon width={20} height={20} />
+          </button>
+          <h1 className="text-lg font-semibold pb-2">
+            Notice from @sunnybiolie
+          </h1>
+          <h3 className="font-medium text-rose-500 pb-3">
+            Something went wrong!
+          </h3>
+          <div className="space-y-2">
+            <p className="indent-3">
+              There is currently an error occurring with Thin Backend that
+              causes pages working with data with Thin Backend to experience
+              irreparable errors. Pages with errors include write, published,
+              and edit pages.
+            </p>
+            <p className="indent-3">
+              {`In the near future, I will wait for the error to be fixed from the
+              Thin Backend side. Since I'm aiming to start another project, if
+              Thin doesn't fix the bug in a short time, I won't be able to
+              switch to another party like Supabase or Neon.`}
+            </p>
+            <p className="indent-3">Sorry for the inconvenience.</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
